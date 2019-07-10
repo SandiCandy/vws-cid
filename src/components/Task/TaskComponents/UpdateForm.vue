3065b518e95bce815fd1b8c98b43c8774d484d49
SandyCandy <sandratieben@gmail.com>
2019-07-10 08:04:50 +0200

    Reloadproblem hoffentlich behoben

--- src/components/Task/TaskComponents/UpdateForm.vue
+++ src/components/Task/TaskComponents/UpdateForm.vue
@@ -1,100 +1,97 @@
 <template>
-      <form class="col-sm-12">
-        <div class="form-group">
-          <label for="title">Name *</label>
-          <input
-            type="text"
-            name="title"
-            id="title"
-            class="form-control"
-            v-model="task.title"
-            v-bind:class="{ 'is-invalid': attemptSubmit && requiredTitle }"
-          >
-          <div class="invalid-feedback">Bitte gib eine Aufgabenbezeichnung an.</div>
-        </div>
+  <form class="col-sm-12">
+    <div class="form-group">
+      <label for="title">Name *</label>
+      <input
+        type="text"
+        name="title"
+        id="title"
+        class="form-control"
+        v-model="task.title"
+        v-bind:class="{ 'is-invalid': attemptSubmit && requiredTitle }"
+      />
+      <div class="invalid-feedback">Bitte gib eine Aufgabenbezeichnung an.</div>
+    </div>
 
-        <div class="form-group">
-          <label for="description">Beschreibung</label>
-          <textarea
-            name="description"
-            id="description"
-            cols="30"
-            rows="5"
-            class="form-control"
-            v-model="task.description"
-          ></textarea>
-        </div>
+    <div class="form-group">
+      <label for="description">Beschreibung</label>
+      <textarea
+        name="description"
+        id="description"
+        cols="30"
+        rows="5"
+        class="form-control"
+        v-model="task.description"
+      ></textarea>
+    </div>
 
-        <div class="form-group">
-          <label for="tasktype_id">Aufgabenart *</label>
-          <select
-            v-model="task.tasktype_id"
-            class="form-control"
-            name="tasktype_id"
-            id="tasktype_id"
-            v-bind:class="{ 'is-invalid': attemptSubmit && requiredTasktype }"
-          >
-            <option v-for="(ttype, index) in tasktypes" :value="ttype.id">{{ ttype.name }}</option>
-          </select>
-          <div class="invalid-feedback">Bitte gib einen Aufgabentypen an.</div>
-        </div>
+    <div class="form-group">
+      <label for="tasktype_id">Aufgabenart *</label>
+      <select
+        v-model="task.tasktype_id"
+        class="form-control"
+        name="tasktype_id"
+        id="tasktype_id"
+        v-bind:class="{ 'is-invalid': attemptSubmit && requiredTasktype }"
+      >
+        <option v-for="(ttype, index) in tasktypes" :value="ttype.id">{{ ttype.name }}</option>
+      </select>
+      <div class="invalid-feedback">Bitte gib einen Aufgabentypen an.</div>
+    </div>
 
-        <div class="form-group">
-          <label for="priority">Priorität *</label>
-          <select v-model="task.priority" class="form-control" name="priority" id="priority">
-            <option value="0">Niedrig</option>
-            <option value="5">Normal</option>
-            <option value="10">Hoch</option>
-          </select>
-        </div>
+    <div class="form-group">
+      <label for="priority">Priorität *</label>
+      <select v-model="task.priority" class="form-control" name="priority" id="priority">
+        <option value="0">Niedrig</option>
+        <option value="5">Normal</option>
+        <option value="10">Hoch</option>
+      </select>
+    </div>
 
-        <div class="form-group">
-          <label for="roomtype_id">Bereich</label>
-          <select v-model="roomtype_id" class="form-control" name="roomtype_id" id="roomtype_id">
-            <option value="0"></option>
-            <option
-              v-for="(rtype, index) in roomtypes"
-              :value="rtype.id"
-            >{{ rtype.name }} ({{ rtype.rooms_count}} Orte)</option>
-          </select>
-        </div>
+    <div class="form-group">
+      <label for="roomtype_id">Bereich</label>
+      <select v-model="roomtype_id" class="form-control" name="roomtype_id" id="roomtype_id">
+        <option value="0"></option>
+        <option
+          v-for="(rtype, index) in roomtypes"
+          :value="rtype.id"
+        >{{ rtype.name }} ({{ rtype.rooms_count}} Orte)</option>
+      </select>
+    </div>
 
-        <div class="form-group" v-if="roomtype_id > 0 || task.room_id">
-          <label for="room_id">Ort</label>
-          <select v-model="task.room_id" class="form-control" name="room_id" id="room_id">
-            <option v-for="(room, index) in rooms" :value="room.id">{{ room.name }}</option>
-          </select>
-        </div>
+    <div class="form-group" v-if="roomtype_id > 0 || task.room_id">
+      <label for="room_id">Ort</label>
+      <select v-model="task.room_id" class="form-control" name="room_id" id="room_id">
+        <option v-for="(room, index) in rooms" :value="room.id">{{ room.name }}</option>
+      </select>
+    </div>
 
-        <div
-          class="form-group"
-          v-if="task.images && task.images.length > 0"
-        >Bildupload nicht verfügbar.</div>
-        <div class="form-group" v-else>
-          <label for="title">Foto</label>
-          <input
-            v-on:change="handleFileUpload"
-            type="file"
-            name="file"
-            id="file"
-            class="form-control"
-            ref="file"
-            accept="image/*"
-            v-bind:class="{ 'is-invalid': attemptSubmit && fileTooLarge }"
-          >
-          <div class="invalid-feedback">Deine Datei ist leider zu groß.</div>
-        </div>
+    <div class="form-group" v-if="task.images && task.images.length > 0">Bildupload nicht verfügbar.</div>
+    <div class="form-group" v-else>
+      <label for="title">Foto</label>
+      <input
+        v-on:change="handleFileUpload"
+        type="file"
+        name="file"
+        id="file"
+        class="form-control"
+        ref="file"
+        accept="image/*"
+        v-bind:class="{ 'is-invalid': attemptSubmit && fileTooLarge }"
+      />
+      <div class="invalid-feedback">Deine Datei ist leider zu groß.</div>
+    </div>
 
-        <div class="form-group" v-if="task.editor_id">
-          <input type="checkbox" id="is_done" name="is_done" v-model="task.is_done">
-          <label for="checkbox">Aufgabe erledigt</label>
-        </div>
+    <div class="form-group" v-if="task.editor_id">
+      <input type="checkbox" id="is_done" name="is_done" v-model="task.is_done" />
+      <label for="checkbox">Aufgabe erledigt</label>
+    </div>
 
-        <div class="form-group">
-          <button type="button" @click="reset" class="btn btn-link text-tudu-blu pl-0">Abbrechen</button>
-          <button type="button" @click="updateTask" class="btn tudu-blu">Änderung speichern</button>
-        </div>
-      </form>
+    <div class="form-group">
+      <button type="button" @click="reset" class="btn btn-link text-tudu-blu pl-0">Abbrechen</button>
+      <button type="button" @click="updateTask" class="btn tudu-blu">Änderung speichern</button>
+    </div>
+  </form>
 </template>
 
 <script>
@@ -103,14 +100,14 @@ export default {
     task: Object,
     roomtype_id: Number
   },
-    data() {
+  data() {
     return {
       success: false,
       error: null,
       tasktypes: [],
       roomtypes: [],
       rooms: [],
-      attemptSubmit: false,
+      attemptSubmit: false
     };
   },
   computed: {
@@ -172,7 +169,6 @@ export default {
           }
         )
         .then(response => {
-          console.log(response.data);
           this.success = true;
           setTimeout(this.reset, 1000);
         })
@@ -257,8 +253,12 @@ export default {
       this.task.title = "";
       this.task.description = "";
       this.task.tasktype_id = "";
-      history.back();
-    },
+      //history.back();
+      this.$router.push({
+        name: "tasks.current",
+        params: { id: this.$route.params.id }
+      });
+    }
   }
 };
 </script>
